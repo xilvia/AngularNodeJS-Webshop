785188bcc98549d4e19d1ad8a0793e701565a94a
Szilvia Kapuvári <kapuvari.szilvia@gmail.com>
2019-08-27 14:45:32 +0200

    minor changes

--- nodejs-angular-webshop-project/server/module/db.js
+++ nodejs-angular-webshop-project/server/module/db.js
@@ -8,7 +8,7 @@ module.exports = class DB {
     // A konstruktor megkapja az adott json fájl nevét.
     constructor(jsonFileName) {
         // Beállítjuk a json fájlokat tartalmazó mappa elérési útját.
-        this.jsonDirectory = path.join(__dirname, './../json');
+        this.jsonDirectory = path.join('./../json');
 
         // Beállítjuk a kezelendő json fájl teljes elérési útját.
         this.jsonFilePath = path.join(
